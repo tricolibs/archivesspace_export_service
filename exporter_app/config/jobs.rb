466c9430c0733124a1b4197e3777f9a06aee5e13
Howard Ding <hding2@brynmawr.edu>
2022-05-09 10:47:04 -0400

    Replace with our real jobs.

--- exporter_app/config/jobs.rb
+++ exporter_app/config/jobs.rb
@@ -1,18 +1,18 @@
 {
   jobs: [
     WeekdayJob.new(:identifier => 'ead',
-                   :description => 'Export EAD versions of resource records',
-                   :days_of_week => ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-                   :start_time => '00:00',
+                   :description => 'Export EAD versions of FHL',
+                   :days_of_week => ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                   :start_time => '01:00',
                    :end_time => '23:59',
-                   :minimum_seconds_between_runs => 5,
+                   :minimum_seconds_between_runs => 10,
 
                    :task => ExportEADTask,
                    :task_parameters => {
                      # Force this job to create a git commit periodically
                      :commit_every_n_records => 50,
                      :search_options => {
-#                       :repo_id => 2,
+                       :repo_id => 7, #FHL
 #                       :identifier => 'AAA.02.G',
 #                       :start_id => 'AAA.01',
 #                       :end_id => 'ZZZ.99',
@@ -23,11 +23,11 @@
                        :numbered_cs => false
                      },
 
-                     :generate_handles => true,
+                     :generate_handles => false,
 
-                     :xslt_transforms => ['config/transform.xslt'],
-                     :validation_schema => ['config/ead.xsd'],
-                     :schematron_checks => ['config/schematron.sch'],
+#                     :xslt_transforms => ['config/transform.xslt'],
+#                     :validation_schema => ['config/ead.xsd'],
+#                     :schematron_checks => ['config/schematron.sch'],
                    },
 
                    :before_hooks => [
@@ -37,22 +37,23 @@
                    :after_hooks => [
 #                     Example showing options that FopPdfGenerator accepts
 #                     FopPdfGenerator.new('config/as-ead-pdf.xsl', :no_git => true, :xconf_file => 'config/fop.xconf'),
-                     FopPdfGenerator.new('config/as-ead-pdf.xsl'),
-                     ErbRenderer.new("templates/manifest.md.erb", "README.md"),
+#                     FopPdfGenerator.new('config/as-ead-pdf.xsl'),
+#                     ErbRenderer.new("templates/manifest.md.erb", "README.md"),
                      ShellRunner.new("scripts/commit_workspace.sh"),
                    ]),
-
-    WeekdayJob.new(:identifier => 'plaintext',
-                   :description => 'Export plaintext versions of resource records',
-                   :days_of_week => ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-                   :start_time => '00:00',
+    WeekdayJob.new(:identifier => 'fhl',
+                   :description => 'Export EAD versions of FHL',
+                   :days_of_week => ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                   :start_time => '01:00',
                    :end_time => '23:59',
-                   :minimum_seconds_between_runs => 5,
+                   :minimum_seconds_between_runs => 10,
 
                    :task => ExportEADTask,
                    :task_parameters => {
+                     # Force this job to create a git commit periodically
+                     :commit_every_n_records => 50,
                      :search_options => {
-#                       :repo_id => 2,
+                       :repo_id => 7, #FHL
 #                       :identifier => 'AAA.02.G',
 #                       :start_id => 'AAA.01',
 #                       :end_id => 'ZZZ.99',
@@ -63,8 +64,11 @@
                        :numbered_cs => false
                      },
 
-                     # Our EAD -> Plaintext XSLT
-                     :xslt_transforms => ['https://raw.githubusercontent.com/saa-ead-roundtable/ead-stylesheets/14d937a237e449874947dcf4b96a9d10ffc6c942/dsc-3-column-table/threecolumn_dsc.xsl'],
+                     :generate_handles => false,
+
+#                     :xslt_transforms => ['config/transform.xslt'],
+#                     :validation_schema => ['config/ead.xsd'],
+#                     :schematron_checks => ['config/schematron.sch'],
                    },
 
                    :before_hooks => [
@@ -72,41 +76,243 @@
                    ],
 
                    :after_hooks => [
-                     ErbRenderer.new("templates/manifest.md.erb", "README.md"),
+#                     Example showing options that FopPdfGenerator accepts
+#                     FopPdfGenerator.new('config/as-ead-pdf.xsl', :no_git => true, :xconf_file => 'config/fop.xconf'),
+#                     FopPdfGenerator.new('config/as-ead-pdf.xsl'),
+#                     ErbRenderer.new("templates/manifest.md.erb", "README.md"),
+                     ShellRunner.new("scripts/commit_workspace.sh"),
+                   ]),
+    WeekdayJob.new(:identifier => 'hc',
+                   :description => 'Export EAD versions of Haverford repository',
+                   :days_of_week => ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                   :start_time => '01:00',
+                   :end_time => '23:59',
+                   :minimum_seconds_between_runs => 10,
+
+                   :task => ExportEADTask,
+                   :task_parameters => {
+                     # Force this job to create a git commit periodically
+                     :commit_every_n_records => 50,
+                     :search_options => {
+                       :repo_id => 5, #Haverford repo
+#                       :identifier => 'AAA.02.G',
+#                       :start_id => 'AAA.01',
+#                       :end_id => 'ZZZ.99',
+                     },
+                     :export_options => {
+                       :include_unpublished => false,
+                       :include_daos => false,
+                       :numbered_cs => false
+                     },
+
+                     :generate_handles => false,
+
+#                     :xslt_transforms => ['config/transform.xslt'],
+#                     :validation_schema => ['config/ead.xsd'],
+#                     :schematron_checks => ['config/schematron.sch'],
+                   },
+
+                   :before_hooks => [
+                     ShellRunner.new("scripts/prepare_workspace.sh"),
+                   ],
+
+                   :after_hooks => [
+#                     Example showing options that FopPdfGenerator accepts
+#                     FopPdfGenerator.new('config/as-ead-pdf.xsl', :no_git => true, :xconf_file => 'config/fop.xconf'),
+#                     FopPdfGenerator.new('config/as-ead-pdf.xsl'),
+#                     ErbRenderer.new("templates/manifest.md.erb", "README.md"),
+                     ShellRunner.new("scripts/commit_workspace.sh"),
+                   ]),
+    WeekdayJob.new(:identifier => 'bmc',
+                   :description => 'Export EAD versions of BMC repository',
+                   :days_of_week => ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                   :start_time => '01:00',
+                   :end_time => '23:59',
+                   :minimum_seconds_between_runs => 10,
+
+                   :task => ExportEADTask,
+                   :task_parameters => {
+                     # Force this job to create a git commit periodically
+                     :commit_every_n_records => 50,
+                     :search_options => {
+                       :repo_id => 6, #BMC repo
+#                       :identifier => 'AAA.02.G',
+#                       :start_id => 'AAA.01',
+#                       :end_id => 'ZZZ.99',
+                     },
+                     :export_options => {
+                       :include_unpublished => false,
+                       :include_daos => false,
+                       :numbered_cs => false
+                     },
+
+                     :generate_handles => false,
+
+#                     :xslt_transforms => ['config/transform.xslt'],
+#                     :validation_schema => ['config/ead.xsd'],
+#                     :schematron_checks => ['config/schematron.sch'],
+                   },
+
+                   :before_hooks => [
+                     ShellRunner.new("scripts/prepare_workspace.sh"),
+                   ],
+
+                   :after_hooks => [
+#                     Example showing options that FopPdfGenerator accepts
+#                     FopPdfGenerator.new('config/as-ead-pdf.xsl', :no_git => true, :xconf_file => 'config/fop.xconf'),
+#                     FopPdfGenerator.new('config/as-ead-pdf.xsl'),
+#                     ErbRenderer.new("templates/manifest.md.erb", "README.md"),
+                     ShellRunner.new("scripts/commit_workspace.sh"),
+                   ]),
+    WeekdayJob.new(:identifier => 'peace',
+                   :description => 'Export EAD versions of Peace repository',
+                   :days_of_week => ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                   :start_time => '01:00',
+                   :end_time => '23:59',
+                   :minimum_seconds_between_runs => 10,
+
+                   :task => ExportEADTask,
+                   :task_parameters => {
+                     # Force this job to create a git commit periodically
+                     :commit_every_n_records => 50,
+                     :search_options => {
+                       :repo_id => 8, #Peace repo
+#                       :identifier => 'AAA.02.G',
+#                       :start_id => 'AAA.01',
+#                       :end_id => 'ZZZ.99',
+                     },
+                     :export_options => {
+                       :include_unpublished => false,
+                       :include_daos => false,
+                       :numbered_cs => false
+                     },
+
+                     :generate_handles => false,
+
+#                     :xslt_transforms => ['config/transform.xslt'],
+#                     :validation_schema => ['config/ead.xsd'],
+#                     :schematron_checks => ['config/schematron.sch'],
+                   },
+
+                   :before_hooks => [
+                     ShellRunner.new("scripts/prepare_workspace.sh"),
+                   ],
+
+                   :after_hooks => [
+#                     Example showing options that FopPdfGenerator accepts
+#                     FopPdfGenerator.new('config/as-ead-pdf.xsl', :no_git => true, :xconf_file => 'config/fop.xconf'),
+#                     FopPdfGenerator.new('config/as-ead-pdf.xsl'),
+#                     ErbRenderer.new("templates/manifest.md.erb", "README.md"),
+                     ShellRunner.new("scripts/commit_workspace.sh"),
+                   ]),
+    WeekdayJob.new(:identifier => 'quaker',
+                   :description => 'Export EAD versions of Quaker Meeting repo',
+                   :days_of_week => ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                   :start_time => '01:00',
+                   :end_time => '23:59',
+                   :minimum_seconds_between_runs => 10,
+
+                   :task => ExportEADTask,
+                   :task_parameters => {
+                     # Force this job to create a git commit periodically
+                     :commit_every_n_records => 50,
+                     :search_options => {
+                       :repo_id => 9, #FHL
+#                       :identifier => 'AAA.02.G',
+#                       :start_id => 'AAA.01',
+#                       :end_id => 'ZZZ.99',
+                     },
+                     :export_options => {
+                       :include_unpublished => false,
+                       :include_daos => false,
+                       :numbered_cs => false
+                     },
+
+                     :generate_handles => false,
+
+#                     :xslt_transforms => ['config/transform.xslt'],
+#                     :validation_schema => ['config/ead.xsd'],
+#                     :schematron_checks => ['config/schematron.sch'],
+                   },
+
+                   :before_hooks => [
+                     ShellRunner.new("scripts/prepare_workspace.sh"),
+                   ],
+
+                   :after_hooks => [
+#                     Example showing options that FopPdfGenerator accepts
+#                     FopPdfGenerator.new('config/as-ead-pdf.xsl', :no_git => true, :xconf_file => 'config/fop.xconf'),
+#                     FopPdfGenerator.new('config/as-ead-pdf.xsl'),
+#                     ErbRenderer.new("templates/manifest.md.erb", "README.md"),
                      ShellRunner.new("scripts/commit_workspace.sh"),
                    ]),
 
-    WeekdayJob.new(:identifier => 'git_repository',
-                   :description => 'Combine repositories for the other jobs and push them up daily',
-                   :days_of_week => ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-                   :start_time => '00:00',
-                   :end_time => '23:59',
-                   :minimum_seconds_between_runs => 5,
 
-                   :task => RepositoryMergeTask,
-                   :task_parameters => {
-                     :jobs_to_merge => ['ead', 'plaintext'],
+#     WeekdayJob.new(:identifier => 'plaintext',
+#                    :description => 'Export plaintext versions of resource records',
+#                    :days_of_week => ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+#                    :start_time => '00:00',
+#                    :end_time => '23:59',
+#                    :minimum_seconds_between_runs => 5,
 
-                     :job_descriptions => {
-                       'ead' => 'Finding aids in EAD (XML) format',
-                       'plaintext' => 'Finding aids in a compact plain text format',
-                     },
+#                    :task => ExportEADTask,
+         #                    :task_parameters => {
+         #                      :search_options => {
+# #                       :repo_id => 2,
+# #                       :identifier => 'AAA.02.G',
+# #                       :start_id => 'AAA.01',
+# #                       :end_id => 'ZZZ.99',
+         #                      },
+         #                      :export_options => {
+#                        :include_unpublished => false,
+#                        :include_daos => false,
+#                        :numbered_cs => false
+         #                      },
 
-                     # Here you can provide a list of additional directories
-                     # whose files we be included at the top-level of the final
-                     # Git repository.  Just create a subdirectory under
-                     # `config` containing your files, and reference them as
-                     # 'config/my_new_subdirectory'.
-                     #
-                     :include_additional_files_from => ['config/readme_and_license'],
+#                      # Our EAD -> Plaintext XSLT
+#                      :xslt_transforms => ['https://raw.githubusercontent.com/saa-ead-roundtable/ead-stylesheets/14d937a237e449874947dcf4b96a9d10ffc6c942/dsc-3-column-table/threecolumn_dsc.xsl'],
+         #                    },
 
-                     # You can use a URL with an embedded username or password like this
-                     :git_remote => 'https://yourusername:yourpassword@github.com/yourusername/yourrepo.git',
+#                    :before_hooks => [
+#                      ShellRunner.new("scripts/prepare_workspace.sh"),
+#                    ],
 
-                     # Or you can use an SSH URL with a Github deploy key
-                     # configured.  See the `bin/generate_deploy_key.sh` script
-                     # for instructions on how to do that.
-                     # :git_remote => 'git@github.com:yourusername/yourrepo.git',
-                   })
+#                    :after_hooks => [
+#                      ErbRenderer.new("templates/manifest.md.erb", "README.md"),
+#                      ShellRunner.new("scripts/commit_workspace.sh"),
+#                    ]),
+
+#     WeekdayJob.new(:identifier => 'git_repository',
+#                    :description => 'Combine repositories for the other jobs and push them up daily',
+#                    :days_of_week => ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+#                    :start_time => '00:00',
+#                    :end_time => '23:59',
+#                    :minimum_seconds_between_runs => 5,
+
+#                    :task => RepositoryMergeTask,
+         #                    :task_parameters => {
+#                      :jobs_to_merge => ['ead', 'plaintext'],
+
+         #                      :job_descriptions => {
+#                        'ead' => 'Finding aids in EAD (XML) format',
+#                        'plaintext' => 'Finding aids in a compact plain text format',
+         #                      },
+
+#                      # Here you can provide a list of additional directories
+#                      # whose files we be included at the top-level of the final
+#                      # Git repository.  Just create a subdirectory under
+#                      # `config` containing your files, and reference them as
+#                      # 'config/my_new_subdirectory'.
+#                      #
+#                      :include_additional_files_from => ['config/readme_and_license'],
+
+#                      # You can use a URL with an embedded username or password like this
+#                      :git_remote => 'https://yourusername:yourpassword@github.com/yourusername/yourrepo.git',
+
+#                      # Or you can use an SSH URL with a Github deploy key
+#                      # configured.  See the `bin/generate_deploy_key.sh` script
+#                      # for instructions on how to do that.
+#                      # :git_remote => 'git@github.com:yourusername/yourrepo.git',
+         #                    })
   ]
 }
