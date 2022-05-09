a6a247dc47dd3e13a2d9f349870dbe329dd1fbbd
Howard Ding <hding2@brynmawr.edu>
2022-05-09 10:46:42 -0400

    Enable this to be configured by ENV variables where we need.

--- exporter_app/config/config.rb
+++ exporter_app/config/config.rb
@@ -1,14 +1,14 @@
 {
-  aspace_username: 'admin',
-  aspace_password: 'admin',
-  aspace_backend_url: 'http://localhost:4567/',
+  aspace_username: ENV['EXPORTER_USERNAME'],
+  aspace_password: ENV['EXPORTER_PASSWORD'],
+  aspace_backend_url: 'http://localhost:8089/',
 
-  handle_wsdl_url: 'http://link.its.yale.edu/ypls-ws/PersistentLinking?wsdl',
-  handle_user: '10079.1/FA',
-  handle_credential: '[YOUR CREDENTIAL]',
-  handle_prefix: '10079.1/fa',
-  handle_group: '10079.1/FA',
-  handle_base: 'http://archives.yale.edu',
+  # handle_wsdl_url: 'http://link.its.yale.edu/ypls-ws/PersistentLinking?wsdl',
+  # handle_user: '10079.1/FA',
+  # handle_credential: '[YOUR CREDENTIAL]',
+  # handle_prefix: '10079.1/fa',
+  # handle_group: '10079.1/FA',
+  # handle_base: 'http://archives.yale.edu',
 
-  log_level: 'debug'
+  log_level: 'warn'
 }
